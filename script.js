9b41fde958db39ebfac25b2326606cd6a3ff3c1e
SietseDEV <110413038+SietseDEV@users.noreply.github.com>
2023-03-18 16:02:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,33 @@
-const API_KEY = 'AIzaSyATnaMLe2oAsrc_ctnzA2CLUZijotYcojg';
-const API_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = "AIzaSyATnaMLe2oAsrc_ctnzA2CLUZijotYcojg";
 const MAX_RESULTS = 20;
 
-const form = document.querySelector('form');
-const videosDiv = document.querySelector('#videos');
+function searchVideos() {
+  const query = document.getElementById("searchInput").value;
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&type=video&part=snippet&maxResults=${MAX_RESULTS}&q=${query}`;
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const searchTerm = event.target.querySelector('input').value;
-  searchVideos(searchTerm);
-});
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const videoContainer = document.getElementById("videoContainer");
+      videoContainer.innerHTML = "";
 
-async function searchVideos(searchTerm) {
-  const response = await fetch(`${API_URL}?part=snippet&maxResults=${MAX_RESULTS}&q=${searchTerm}&type=video&key=${API_KEY}`);
-  const data = await response.json();
-  showVideos(data.items);
-}
+      data.items.forEach((item) => {
+        const videoId = item.id.videoId;
+        const title = item.snippet.title;
+        const thumbnailUrl = item.snippet.thumbnails.medium.url;
+        const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
 
-function showVideos(videos) {
-  videosDiv.innerHTML = '';
-  videos.forEach(video => {
-    const videoDiv = document.createElement('div');
-    videoDiv.classList.add('video');
+        const videoCard = `
+          <div class="video-card">
+            <a href="${videoUrl}" target="_blank"><img src="${thumbnailUrl}" alt="${title}"></a>
+            <h3><a href="${videoUrl}" target="_blank">${title}</a></h3>
+          </div>
+        `;
 
-    if (video.id.videoId) {
-      videoDiv.innerHTML = `
-        <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${video.id.videoId}" frameborder="0" allowfullscreen></iframe>
-      `;
-    } else if (video.id.channelId) {
-      videoDiv.innerHTML = `
-        <h2>${video.snippet.title}</h2>
-        <div class="channel">
-          <img src="${video.snippet.thumbnails.default.url}" alt="${video.snippet.channelTitle} logo">
-          <p>${video.snippet.channelTitle}</p>
-        </div>
-        <p class="description">${video.snippet.description}</p>
-      `;
-    } else {
-      videoDiv.innerHTML = `
-        <h2>${video.snippet.title}</h2>
-        <div class="channel">
-          <img src="${video.snippet.thumbnails.default.url}" alt="${video.snippet.channelTitle} logo">
-          <p>${video.snippet.channelTitle}</p>
-        </div>
-        <div class="embed">${video.snippet.embedHtml}</div>
-      `;
-    }
-
-    const viewsDiv = document.createElement('div');
-    viewsDiv.classList.add('views');
-    viewsDiv.textContent = `${formatNumber(video.statistics.viewCount)} views`;
-
-    const likesDiv = document.createElement('div');
-    likesDiv.classList.add('likes');
-    likesDiv.textContent = `${formatNumber(video.statistics.likeCount)} likes`;
-
-    videoDiv.appendChild(viewsDiv);
-    videoDiv.appendChild(likesDiv);
-    videosDiv.appendChild(videoDiv);
-  });
-}
-
-function formatNumber(number) {
-  if (number >= 1000000) {
-    return `${(number / 1000000).toFixed(1)}M; } else if (number >= 1000) { return ${(number / 1000).toFixed(1)}K`;
-    } else {
-      return number;
-    }
+        videoContainer.insertAdjacentHTML("beforeend", videoCard);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
