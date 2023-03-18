1863efd0ce7fe0f5d1f7e1c8e734eb0cb88cc883
SietseDEV <110413038+SietseDEV@users.noreply.github.com>
2023-03-18 16:06:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,18 +16,29 @@ function searchVideos() {
         const title = item.snippet.title;
         const thumbnailUrl = item.snippet.thumbnails.medium.url;
         const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+        const channelTitle = item.snippet.channelTitle;
+        const channelId = item.snippet.channelId;
+        const channelImageUrl = item.snippet.thumbnails.default.url;
+        const views = item.statistics?.viewCount || 'N/A';
 
         const videoCard = `
           <div class="video-card">
-            <a href="${videoUrl}" target="_blank"><img src="${thumbnailUrl}" alt="${title}"></a>
-            <h3><a href="${videoUrl}" target="_blank">${title}</a></h3>
+            <a href="${videoUrl}">
+              <img src="${thumbnailUrl}" alt="${title}">
+              <h3>${title}</h3>
+            </a>
+            <div class="channel-info">
+              <img src="${channelImageUrl}" alt="${channelTitle} profile picture">
+              <div>
+                <p>${channelTitle}</p>
+                <p>${views} views</p>
+              </div>
+            </div>
           </div>
         `;
 
-        videoContainer.insertAdjacentHTML("beforeend", videoCard);
+        videoContainer.innerHTML += videoCard;
       });
     })
-    .catch((error) => {
-      console.log(error);
-    });
+    .catch((error) => console.error(error));
 }
